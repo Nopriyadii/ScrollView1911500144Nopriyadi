65fc7b8a7d9a5c831fabb18b2c4a15d6fa63dedb
Nopriyadii <1911500144@mahasiswa.atmaluhur.ac.id>
2021-12-20 20:53:52 -0800

    upload

---
+++ app/src/main/java/com/example/scrollview1911500144_nopriyadi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scrollview1911500144_nopriyadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
